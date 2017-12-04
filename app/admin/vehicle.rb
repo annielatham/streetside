5d7b9dead4c45f7b3f5ffe19efb4120f5254c199
Raghu Betina <raghu@firstdraft.com>
2017-12-04 22:19:13 +0000

    Whitelist columns in Vehicle for ActiveAdmin

--- app/admin/vehicle.rb
+++ app/admin/vehicle.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Vehicle do
 
+ permit_params :year, :make, :model, :color, :nickname, :license_plate
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
