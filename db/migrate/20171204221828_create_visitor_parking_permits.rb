783ec6c5e3b599a8cec95808dd67408afaf9755a
Raghu Betina <raghu@firstdraft.com>
2017-12-04 22:18:28 +0000

    Generate visitor_parking_permits resource with starter:resource

---
+++ db/migrate/20171204221828_create_visitor_parking_permits.rb
@@ -0,0 +1,12 @@
+class CreateVisitorParkingPermits < ActiveRecord::Migration
+  def change
+    create_table :visitor_parking_permits do |t|
+      t.integer :resident_id
+      t.date :purchase_date
+      t.date :expiration_date
+
+      t.timestamps
+
+    end
+  end
+end
