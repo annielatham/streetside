8022260775c7a7bb393921dd84154017e64cb235
Raghu Betina <raghu@firstdraft.com>
2017-12-04 22:18:48 +0000

    Direct association between users and visitor_parking_permits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :visitor_parking_permits,
+             :foreign_key => "resident_id",
+             :dependent => :nullify
+
   has_many   :resident_parking_permits,
              :foreign_key => "resident_id",
              :dependent => :nullify
--- app/models/visitor_parking_permit.rb
+++ app/models/visitor_parking_permit.rb
@@ -1,6 +1,11 @@
 class VisitorParkingPermit < ApplicationRecord
   # Direct associations
 
+  belongs_to :resident_host,
+             :class_name => "User",
+             :foreign_key => "resident_id",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
