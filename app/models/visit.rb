4e348f39b3c920cd47d6c3f58027dac0b04a976c
Raghu Betina <raghu@firstdraft.com>
2017-12-04 22:18:55 +0000

    Direct association between visitor_parking_permits and visits

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,10 @@
 class Visit < ApplicationRecord
   # Direct associations
 
+  belongs_to :visitor_permit,
+             :class_name => "VisitorParkingPermit",
+             :foreign_key => "permit_id"
+
   belongs_to :visiting_vehicle,
              :class_name => "Vehicle",
              :foreign_key => "vehicle_id",
--- app/models/visitor_parking_permit.rb
+++ app/models/visitor_parking_permit.rb
@@ -1,6 +1,11 @@
 class VisitorParkingPermit < ApplicationRecord
   # Direct associations
 
+  has_many   :assigned_visits,
+             :class_name => "Visit",
+             :foreign_key => "permit_id",
+             :dependent => :destroy
+
   belongs_to :resident_host,
              :class_name => "User",
              :foreign_key => "resident_id",
